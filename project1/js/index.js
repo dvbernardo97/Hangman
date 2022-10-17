17dbb2f3fb995f72d8f8e028906d5b10a8fc1d5f
dominic bernardo <dominicvbern@gmail.com>
2022-10-16 17:11:25 -0700

    switch

---
+++ project1/js/index.js
@@ -0,0 +1,8 @@
+// choosing random word from word bank
+WordBank = {}
+// keyclick interaction/event listener
+
+// word display
+// number of lines for the word
+// showing which letters where picked
+// set containers for all these options
