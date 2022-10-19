164fac49091cf2899297e8b589ec50df9abd2dd2
dominic bernardo <dominicvbern@gmail.com>
2022-10-19 15:13:55 -0700

    guesses

--- project1/js/index.js
+++ project1/js/index.js
@@ -3,9 +3,11 @@ const inputs = document.querySelector(".inputs")
 const incorrect = document.querySelector(".wrong")
 const resetbtn = document.querySelector('.resetbttn')
 const hint = document.querySelector('.hint span')
+const guessLeft = document.querySelector('.guesses span')
 const typingInput = document.querySelector('.typing-input')
 const guessBtn = document.querySelector('.guessBttn')
 let wrong = []
+let guesses = []
 // creating a word bank
 
 const wordBank = ['twix', 'snickers', 'skittles']
@@ -17,16 +19,16 @@ function getRandomInt(max) {
 const randomIndex = getRandomInt(wordBank.length)
 
 let word = wordBank[randomIndex]
-
+guesses = 8;
 // figure out how many letters are in the word
 let box = ""
 for (let i = 0; i < word.length; i++) {
     box += '<input type="text" disabled>';
 }
 inputs.innerHTML = box, word.length;
+guessLeft.innerText = guesses
 
-
-// // // how many letters remain to be guessed
+// how many letters remain to be guessed
 function pInput(e) {
     let key = typingInput.value
     if (key.match(/^[A-Za-z]+$/)) {
@@ -38,21 +40,27 @@ function pInput(e) {
                 }
             }
         } else {
+            guesses--;
             // if letter is wrong display and present a body part
             wrong.push(key)
+
+
         }
     }
+    guessLeft.innerText = guesses
     incorrect.innerText = wrong
+
+    if (guesses < 1) {
+        alert("Game Over! No more guesses")
+        for (let i = 0; i < word.length; i++) {
+            inputs.querySelectorAll("input")[i].value = word[i]
+        }
+    }
 }
+// Winning and losing functions
 
-// // //  showing which letters where picked
-
-// // //if letter is right replace dash with the letter
-
-
-// // // Winning and losing functions
-// // //how to determine the winner
-// // // how to determine the loser
+//how to determine the winner
+// how to determine the loser
 
 // keyclick interaction/event listener
 resetbtn.addEventListener('click', getRandomInt) // reset button
