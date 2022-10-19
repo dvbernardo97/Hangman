9f4009fbe34a0de0620065cb4a5bd26d30023078
dominic bernardo <dominicvbern@gmail.com>
2022-10-19 00:37:26 -0700

    values

--- project1/js/index.js
+++ project1/js/index.js
@@ -1,6 +1,6 @@
 // Get elements
 const inputs = document.querySelector(".inputs")
-const wordGrab = document.getElementById('word')
+// const wordGrab = document.getElementById('word')
 const resetbtn = document.querySelector('.resetbttn')
 const hint = document.querySelector('.hint span')
 const typingInput = document.querySelector('.typing-input')
@@ -17,39 +17,50 @@ const randomIndex = getRandomInt(wordBank.length)
 
 let word = wordBank[randomIndex]
 
-// replace each letter with an underscore
+let box = ""
+for (let i = 0; i < word.length; i++) {
+    box += '<input type="text" disabled>';
+}
+inputs.innerHTML = box, word.length;
 
-wordGrab.innerHTML = "____ ".repeat(word.length)
+// random();
 
-// how many letters remain to be guessed
+// // replace each letter with an underscore
+
+// inputs.innerText = "__ ".repeat(word.length, box)
+
+// // // how many letters remain to be guessed
 function pInput(e) {
-    let key = e.target.value
+    let key = typingInput.value
     if (key.match(/^[A-Za-z]+$/)) {
         console.log(key)
         if (word.includes(key)) {
-            for (word[i] === key) 
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] === key) {
+                    inputs.querySelectorAll("input")[i].value = key
+                }
+            }
         } else {
             console.log("letter not found")
         }
     }
 }
-// figure out how many letters are in the word
-// display that number of underscores
+// // // figure out how many letters are in the word
+// // // display that number of underscores
 
-// keyclick interaction/event listener
-
-
-
-//  showing which letters where picked
-// if letter is wrong display and present a body part
-//if letter is right replace dash with the letter
+// // // keyclick interaction/event listener
 
 
-// Winning and losing functions
-//how to determine the winner
-// how to determine the loser
-// reset button
-resetbtn.addEventListener('click', wordBank)
+
+// // //  showing which letters where picked
+// // // if letter is wrong display and present a body part
+// // //if letter is right replace dash with the letter
+
+
+// // // Winning and losing functions
+// // //how to determine the winner
+// // // how to determine the loser
+// // // reset button
+resetbtn.addEventListener('click', getRandomInt)
 document.addEventListener('keydown', () => typingInput.focus())
-typingInput.addEventListener("input", pInput)
-// guessBtn.addEventListener('click', () => console.log(typingInput.value))
+guessBtn.addEventListener("click", pInput)
